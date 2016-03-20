05e7c8f60129f1b5274e464ddbf0b9fb12fd5f3a
Sahil Ahuja <sahilahuja@gmail.com>
2016-03-20 12:58:45 +0530

    Adding stuff

--- loadsensor/html/onload.js
+++ loadsensor/html/onload.js
@@ -1,10 +1,13 @@
 console.log('here');
 
-$.get("/output.json", function (data, status) {
-    console.log('Amount of data: ', data.length);
-    $.each(data, function(key, value){
-        console.log(key, value);
-
-        $("#shopping_list").append('<li>container '+value.containerNo+' has weight '+value.weight+'</li>');
+function updateData() {
+    $.get("/output.json", function (data, status) {
+        console.log('Amount of data: ', data.length);
+        $.each(data, function(key, value){
+            console.log(key, value);
+            $("#shopping_list").append('<li>container '+value.containerNo+' has weight '+value.weight+'</li>');
+        });
     });
-});
+}
+
+setTimeout(updateData, 5);
