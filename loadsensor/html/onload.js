154e8b87aba4f17ae47dea9dc53d4f33aa7a17bd
Sahil Ahuja <sahilahuja@gmail.com>
2016-03-20 13:06:08 +0530

    Adding stuff

--- loadsensor/html/onload.js
+++ loadsensor/html/onload.js
@@ -4,10 +4,10 @@ function updateData() {
     console.log('Updating data: ');
     $.get("/output.json", function (data, status) {
         console.log('Amount of data: ', data.length);
+        var $div = $('#shopping_list');
         $div.contents().remove();
         $.each(data, function(key, value){
             console.log(key, value);
-            var $div = $('#shopping_list');
             $div.append('<li>container '+value.containerNo+' has weight '+value.weight+'</li>');
         });
     });
